83f9a851e5405dcacc3a5890c3c570aab9f0066e
Rahul <rbhat5480@gmail.com>
2024-11-12 22:32:06 +0530

    Intial Commit

--- server/helpers/cloudinary.js
+++ server/helpers/cloudinary.js
@@ -2,9 +2,9 @@ const cloudinary = require("cloudinary").v2;
 const multer = require("multer");
 
 cloudinary.config({
-  cloud_name: "***REMOVED***",
-  api_key: "***REMOVED***",
-  api_secret: "***REMOVED***",
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 const storage = new multer.memoryStorage();
--- server/helpers/paypal.js
+++ server/helpers/paypal.js
@@ -2,8 +2,8 @@ const paypal = require("paypal-rest-sdk");
 
 paypal.configure({
   mode: "sandbox",
-  client_id: "***REMOVED***",
-  client_secret: "***REMOVED***",
+  client_id: process.env.PAYPAL_CLIENT_ID,
+  client_secret: process.env.PAYPAL_CLIENT_SECRET,
 });
 
 module.exports = paypal;
